41ee6530c474a09c6309eed9d66a57b2b6e546f9
Hweedong Yoo <yoojon675@gmail.com>
2024-08-13 18:49:58 -0700

    Initialize setup

---
+++ client/src/pages/Tasks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div>
+            Tasks
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ client/src/pages/TasksDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TasksDetails = () => {
+    return (
+        <div>
+            TasksDetails
+        </div>
+    )
+}
+
+export default TasksDetails
