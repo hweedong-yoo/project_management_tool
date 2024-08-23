89f5ef416d4e5e9c1785cf945ad8206d1df3d4cf
Hweedong Yoo <yoojon675@gmail.com>
2024-08-23 00:49:08 -0700

    Remove syntax errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import { Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom'
-import Toaster from 'sonner'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import Tasks from './pages/Tasks'
-import Users from './pages/Users'
-import Trash from './pages/Trash'
-import TaskDetails from './pages/TaskDetails'
+import { Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Toaster } from 'sonner';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Tasks from './pages/Tasks';
+import Users from './pages/Users';
+import Trash from './pages/Trash';
+import TasksDetails from './pages/TasksDetails';
 
 function Layout() {
-  const user = ""
-  const location = useLocation()
+  const user = "";
+  const location = useLocation();
   return user ? (
     <div className="w-full h-screen flex flex-col md:flex-row">
       <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
@@ -21,13 +21,13 @@ function Layout() {
       <div className='flex-1 overflow-y-auto'>
         {/* {<Navbar/>} */}
         <div className='p-4 2xl:px-10'>
-          <Outlet/>
+          <Outlet />
         </div>
       </div>
     </div>
   ) : (
     <Navigate to="/log-in" state={{ from: location }} replace />
-  )
+  );
 }
 
 function App() {
@@ -36,23 +36,23 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path='/' element={<Navigate to='/dashboard' />} />
-          <Route path='/dashboard >' element={<Dashboard />} />
-          <Route path='/tasks >' element={<Tasks />} />
-          <Route path='/completed/:status >' element={<Tasks />} />
-          <Route path='/in-progress/:status >' element={<Tasks />} />
-          <Route path='/todo/:status >' element={<Tasks />} />
-          <Route path='/team >' element={<Users />} />
-          <Route path='/trashed >' element={<Trash />} />
-          <Route path='/task/:id >' element={<TaskDetails />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='/completed/:status' element={<Tasks />} />
+          <Route path='/in-progress/:status' element={<Tasks />} />
+          <Route path='/todo/:status' element={<Tasks />} />
+          <Route path='/team' element={<Users />} />
+          <Route path='/trashed' element={<Trash />} />
+          <Route path='/task/:id' element={<TasksDetails />} />
         </Route>
 
-        <Route path='/log-in>' element={<Login />} />
+        <Route path='/log-in' element={<Login />} />
 
       </Routes>
 
       <Toaster richColor />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
